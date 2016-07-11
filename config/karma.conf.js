97d348e9a5855df22afcd20fcdabe9c307a5d12b
Arjan van Wijk <thanarie@gmail.com>
2016-07-12 00:30:21 +0200

    Add sourcemap support for coverage by using remap-istanbul
    Added remap-istanbul and all supporting karma plugins to correctly
    remap the coverage results back to their TypeScript source.
    
    Since all generations happens in memory by Webpack, we needed inline
    sourcemap support, provided by the Karma plugins, the Webpack devtool
    setting, and the webpack typescript loader.
    
    The normal `ts-loader` doesn't support inline sourcemaps, so we switched
    to `awesome-typescript-loader`. We created a new `tsconfig.test.json`
    that is the same as the normal `tsconfig.json` but enables inline
    sourcemaps by adding `"inlineSourceMap": true`.
    
    The webpack dist builds have also switched to `awesome-typescript-loader`
    to keep the configuration consistent.
    
    To make the `awesome-typescript-loader` correctly work with const enums,
    we needed to enable `preserveConstEnums` property in the `tsconfig.json`.
    
    We also switched he `test/index.js` to `index.ts` and included
    `webpack-env` in the `typings.json` so all wepack requires work in
    TypeScript.
    
    To have correct coverage reports consisting of _all_ the source files,
    we've added a `Dummy.ts` file in the `src/` folder that is not
    referenced by any tests, but must show up in the coverage results.
    If this is not the case, we know our test setup is incorrect.
    
    re #15

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,88 +1,87 @@
-var path = require('path');
-var webpackConfig = require('./webpack.config')();
-
-webpackConfig.module.postLoaders = [
-	// instrument only testing sources with Istanbul
-	{
-		test: /\.ts$/,
-		include: path.resolve('src/'),
-		loader: 'istanbul-instrumenter'
-	}
-];
-
-module.exports = function (config)
+/**
+ * Please see Karma config file reference for better understanding:
+ * http://karma-runner.github.io/latest/config/configuration-file.html
+ */
+module.exports = function(config)
 {
 	config.set({
 
 		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '../',
 
-
-		// frameworks to use
+		// List of test frameworks we will use. Most of them are provided by separate packages (adapters).
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['mocha', 'chai'],
+		frameworks: ['mocha', 'chai', 'source-map-support'],
 
-		// list of files / patterns to load in the browser
+		// Entry point / test environment builder is also written in TypeScript.
 		files: [
-			'test/index.js'
+			'./test/index.ts'
 		],
-
 
 		// list of files to exclude
 		exclude: [],
 
-
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'test/index.js': ['webpack'],
-			'src/*.ts': ['webpack','coverage'] // only for webstorm, real coverage is injected as webpack-loader
+			'./src/**/*.ts': [
+				'webpack',
+				'sourcemap',
+				'coverage'
+			],
+			'./test/**/*.ts': [
+				'webpack'
+			]
 		},
 
-		webpack: {
-			module : webpackConfig.module,
-			resolve : webpackConfig.resolve
-		},
+		webpack: require('./webpack.config.test')(),
 
+		// Make dev server silent.
+		webpackServer: { noInfo: true },
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'coverage'],
+		// A lot of plugins are available for test results reporting.
+		// You can find them here: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
 
+		// Simple summary (printed to the console) and JSON file which we will remap back to TypeScript.
 		coverageReporter: {
 			dir: 'coverage',
 			reporters: [
 				// reporters not supporting the `file` property
-				{ type: 'json', dir: 'coverage', subdir: '.' },
-				{ type: 'lcov', dir: 'coverage', subdir: '.' },
+				{ type: 'json', subdir: '.' },
+				// { type: 'lcov', subdir: '.' },
 
 				{ type: 'text' }
 			]
 		},
 
-
-		// web server port
-		port: 9876,
-
-
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
-
+		// Map code coverage result back to TypeScript using `karma-remap-istanbul`.
+		remapIstanbulReporter: {
+			src: 'coverage/coverage-final.json',
+			reports: {
+				lcovonly: 'coverage/lcov.info',
+				html: 'coverage/report'
+			},
+			timeoutNotCreated: 5000,
+			timeoutNoMoreFiles: 1000
+		},
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
 
-
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
-
-
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['PhantomJS'],
 
+		// web server port
+		port: 9876,
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
@@ -92,4 +91,4 @@ module.exports = function (config)
 		// how many browser should be started simultaneous
 		concurrency: Infinity
 	})
-}
+};
--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,17 @@
 // require all test files
-const testsContext = require.context('./', true, /Spec\.ts$/);
+const testsContext = require.context(
+	'./',
+	true,
+	/Spec\.ts/
+);
 
 testsContext.keys().forEach(testsContext);
 
 // require all source files
-const sourcesContext = require.context('../src/', true, /\.ts$/);
+const sourcesContext = require.context(
+	'../src/',
+	true,
+	/\.ts$/
+);
 
 sourcesContext.keys().forEach(sourcesContext);
