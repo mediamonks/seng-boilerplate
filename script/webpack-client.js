cebc58bd13fb09e238443ff1505769f3a489fe01
Victor Garrido <v.garrido@mediamonks.com>
2016-10-14 14:31:18 +0200

    Add webpack-client.js allowing testing in browser.

---
+++ script/webpack-client.js
@@ -0,0 +1,40 @@
+const webpack = require("webpack");
+const webpackDevServer = require('webpack-dev-server');
+const path = require('path');
+const baseConfig = require('../config/webpack.config.dist');
+
+const port = 8080;
+const serverURI = `webpack-dev-server/client?http://localhost:${port}/`;
+
+let browser = baseConfig();
+browser.output.libraryTarget = "var";
+browser.output.filename = "./dist/seng-boilerplate.js";
+browser.output.path = path.join(__dirname, '../dist');
+
+if(typeof browser.entry === 'string')
+{
+	const { entry }  = browser;
+	browser.entry = { app: [serverURI, entry]};
+}
+else if(typeof browser.entry.app === 'object')
+{
+	browser.entry.app.unshift(serverURI)
+}
+
+browser.devtool = 'source-map';
+browser.watch = true;
+browser.progress = true;
+browser.keepalive = true;
+
+const compiler = webpack(browser);
+const server = new webpackDevServer(compiler, {
+	contentBase: "example/"
+});
+
+server.listen(port, function (err) {
+	if (err) {
+		console.log(err);
+		return
+	}
+	console.log(`Listening at http://localhost:${port}\n`);
+});
