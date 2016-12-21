e47f2aed9473e5048307687331588ac3e7f875d1
Mient-jan Stelling <mientjan.stelling@gmail.com>
2016-12-21 11:03:00 +0100

    - adding tslib  - enabling importHelpers in tsconfig

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 // require all test files
-const testsContext = require.context(
+const testsContext = (<any> require).context(
 	'./',
 	true,
 	/Spec\.ts/
@@ -8,7 +8,7 @@ const testsContext = require.context(
 testsContext.keys().forEach(testsContext);
 
 // require all source files
-const sourcesContext = require.context(
+const sourcesContext = (<any> require).context(
 	'../src/',
 	true,
 	/\.ts$/
